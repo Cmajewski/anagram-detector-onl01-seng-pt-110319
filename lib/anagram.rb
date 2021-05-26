ca0fdb569273fd6810dfabe6b3fe9b30ca9622c9
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-05-26 16:19:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ attr_accessor :word
   end
 
   def match(phrase)
-    phrase.split(" ").each do |seperated_words|
+    phrase.each do |seperated_words|
       if seperated_word.split(" ").sort==word.split(" ").sort
         seperated_word
       end
